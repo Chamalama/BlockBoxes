9e4694c0048382d90862a782a8e8075d5a27dde5
BuildTools <unconfigured@null.spigotmc.org>
2024-05-01 06:50:42 -0400

    f

--- src/main/java/com/cham/blockboxes/Util/ItemUtil.java
+++ src/main/java/com/cham/blockboxes/Util/ItemUtil.java
@@ -19,18 +19,4 @@ public class ItemUtil {
         return is;
     }
 
-    public static ItemStack buildMoneyGemNormal(long amount) {
-        return buildItem(Material.DIAMOND, ChatColor.AQUA + ChatColor.BOLD.toString() + "Money Gem " + ChatColor.GRAY + "[" + ChatColor.AQUA + format(amount) + ChatColor.GRAY + "]",
-                ChatColor.GRAY + "(Right Click) " + ChatColor.WHITE + "To redeem this " + ChatColor.AQUA + "Gem",
-                        ChatColor.WHITE + "and receive " + ChatColor.AQUA + "$" + format(amount));
-    }
-
-    public static String format(long format) {
-        DecimalFormat decimalFormat = new DecimalFormat("#,###");
-        return decimalFormat.format(format);
-    }
-
-    public static boolean isMoneyGem(ItemStack itemStack) {
-        return itemStack.hasItemMeta() && itemStack.getItemMeta().hasDisplayName() && itemStack.getItemMeta().getDisplayName().contains(ChatColor.AQUA + ChatColor.BOLD.toString() + "Money Gem ");
-    }
 }
